c62e8e1fe8de3327cd7023dd1abbeca156a718b6
K <25370141+SherkinOne@users.noreply.github.com>
2019-04-07 17:47:57 +0100

    Now to add saving features to firebase

--- main.dart
+++ main.dart
@@ -1,119 +1,124 @@
 import 'dart:async';
 import 'dart:convert';
 import 'package:flutter/material.dart';
-import "package:pull_to_refresh/pull_to_refresh.dart";
 import 'package:http/http.dart' as http;
 
 void main() => runApp(MyApp());
 
-class MyApp extends StatelessWidget {  
-  MyApp({Key key, this.questions}) : super(key: key);
-  final String questions;
-  var no;
-  bool isSaved;
+class MyApp extends StatelessWidget {
   @override
-  
   Widget build(BuildContext context) {
     return MaterialApp(
-      title: 'Trivia....',
+      title: 'Pub Quiz',
       theme: ThemeData(
-        primarySwatch: Colors.green,
+        primarySwatch: Colors.blue,
       ),
-      home: Scaffold(
-        appBar: AppBar(
-          title: Text('Pub Quiz Questions'),
-          centerTitle: true,
-        ),
-        
-        body:   Container (
-          child : FutureBuilder(
-          future: _getQuestion(), //sets the getQuote method as the expected Future
-            builder: (BuildContext context, AsyncSnapshot snapshot) {    
-            if (snapshot.hasError) {
-              return Text('Error: ${snapshot.error}');
-            } else {  
-              if (snapshot.hasData){          
-              return  new SmartRefresher(
-                    enablePullDown: true,
-                    enablePullUp: false,
-                    controller : RefreshController(),
-                child : ListView.builder(               
-                itemCount : snapshot.data.length,
-                scrollDirection: Axis.vertical, 
-                itemBuilder :(BuildContext context, int index){
-                   no=index+1;
-                  return ExpansionTile(              
-                    title: Text("${no}. "+snapshot.data[index].question, style: TextStyle(fontSize: 20.0, fontWeight: FontWeight.bold, fontStyle: FontStyle.italic),),
-                     children: <Widget>[
-                      Column(
-                        children: _buildExpandableContent(snapshot.data[index]),
-                        
-              )]                             
-              );
-            }
-            ));
-              }
-            else {
-             return Center(
-               child : CircularProgressIndicator()
-             );
-            }
-      }}     
-      )
-     ))
+      home: MyHomePage(title: 'Pub Quiz Questions'),
     );
-    }}
-
-      _buildExpandableContent(Questions questions) {
-        // This is the expandable part of the tile that shows the answer
-    List<Widget> columnContent = [];
-      columnContent.add(
-        ListTile(
-          title: Text(questions.answer, style: TextStyle(fontSize: 18.0),),
-        // need a star which add to a saved list (maybe offline??)
-         
-      ));
-
-            columnContent.add(
-              IconButton(
-                icon : Icon(Icons.save),
-                onPressed : saveQuestion()
-        )
-      );
-    return columnContent;
   }
-
-loadQuestionList(){
-
-  
 }
 
-  saveQuestion(){
-      // save the question to firebase
-      // can be accessible by the other page
-    return;
+class MyHomePage extends StatefulWidget {
+  MyHomePage({Key key, this.title}) : super(key: key);
+  final String title;
+  List questions = [];
+  @override
+  _MyHomePageState createState() => _MyHomePageState();
+}
+
+class _MyHomePageState extends State<MyHomePage> {
+  var savedQuestions;
+  var x = 0;
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+        appBar: AppBar(
+            backgroundColor: Colors.deepPurple[300],
+            centerTitle: true,
+            title: (Text(widget.title))),
+        body: Container(
+            child: FutureBuilder(
+                future:
+                    _getQuestion(), //sets the getQuestions method as the expected Future
+                builder: (BuildContext context, AsyncSnapshot snapshot) {
+                  if (snapshot.hasError) {
+                    return Text('Error: ${snapshot.error}');
+                  } else {
+                    if (snapshot.hasData) {
+                      return RefreshIndicator(
+                          // could call function that changes state relative to the parameter passed??
+                          onRefresh: () async {
+                            //async needed else whirly circle stays
+                            setState(() {
+                              // _getQuestion();
+                              return;
+                            });
+                          },
+                          child: ListView.builder(
+                              itemCount: snapshot.data.length,
+                              scrollDirection: Axis.vertical,
+                              itemBuilder: (BuildContext context, int index) {
+                                var no = index + 1;
+                                return ExpansionTile(
+                                    title: Text(
+                                      "$no. " + snapshot.data[index].question,
+                                      style: TextStyle(
+                                          fontSize: 20.0,
+                                          fontWeight: FontWeight.bold,
+                                          fontStyle: FontStyle.italic),
+                                    ),
+                                    children: <Widget>[
+                                      Column(
+                                        children: _buildExpandableContent(
+                                            snapshot.data[index]),
+                                      )
+                                    ]);
+                              }));
+                    } else {
+                      return Center(child: CircularProgressIndicator());
+                    }
+                  }
+                })));
   }
 
-  Future<List<Questions>> _getQuestion() async {
-    String url = 'http://jservice.io/api/random?count=20';
+  _buildExpandableContent(Questions questions) {
+    /// This is the expandable part of the tile that shows the answer
+    List<Widget> columnContent = [];
 
-    final response =
-        await http.get(url, headers: {"Accept": "application/json"});
-    
-    if (response.statusCode == 200) {
-      var theQuestions = jsonDecode(response.body);
-      print(response.body);
-      List<Questions> quest = [];   
-        for (var q in theQuestions){
-          Questions questi = Questions(q['question'], q["answer"],false);
-          quest.add(questi);
-        }
-      return quest;
-    } else {
-        throw Exception('Failed to load post');
-   }
+    columnContent.add(ListTile(
+      title: Text(
+        questions.answer,
+        style: TextStyle(fontSize: 18.0),
+      ),
+    ));
+    columnContent.add(
+      TapboxA(theQuestion: questions.question, theAnswer: questions.answer),
+    );
+
+    return columnContent;
   }
-  
+}
+
+loadQuestionList() {}
+
+Future<List<Questions>> _getQuestion() async {
+  String url = 'http://jservice.io/api/random?count=20';
+
+  final response = await http.get(url, headers: {"Accept": "application/json"});
+
+  if (response.statusCode == 200) {
+    var theQuestions = jsonDecode(response.body);
+    print(response.body);
+    List<Questions> quest = [];
+    for (var q in theQuestions) {
+      Questions questi = Questions(q['question'], q["answer"], false);
+      quest.add(questi);
+    }
+    return quest;
+  } else {
+    throw Exception('Failed to load post');
+  }
+}
 
 class Questions {
   Questions(this.question, this.answer, this.saved);
@@ -122,8 +127,50 @@ class Questions {
   String answer;
   bool saved;
 
-  Questions.fromJson(List <dynamic> json) :
-       question = 'question',
+  Questions.fromJson(List<dynamic> json)
+      : question = 'question',
         answer = 'answer',
         saved = false;
+}
+
+class SavedQuestions {
+  SavedQuestions(this.question, this.answer);
+
+  String question;
+  String answer;
+}
+
+class TapboxA extends StatefulWidget {
+  TapboxA({Key key, this.theQuestion, this.theAnswer}) : super(key: key);
+  final String theQuestion;
+  final String theAnswer;
+  @override
+  _TapboxAState createState() => _TapboxAState();
+}
+
+class _TapboxAState extends State<TapboxA> {
+  bool savedOrNot = false;
+
+  _handleTap() {
+    setState(() {
+      print(savedOrNot);
+      savedOrNot = !savedOrNot;
+      if (!savedOrNot){
+      print(widget.theQuestion);
+      // save to other class
+      // or maybe save direct to firebase
+      }
+
+    });
+  }
+
+  Widget build(BuildContext context) {
+    return IconButton(
+      icon: Icon(Icons.star),
+      color: savedOrNot ? Colors.red : Colors.green,
+      splashColor: Colors.green,
+      highlightColor: Colors.yellow,
+      onPressed: _handleTap,
+    );
+  }
 }
